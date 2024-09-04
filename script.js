3916c6f7dde7572ddf0c0ba9d0c5bbf990142da3
NatesanG <161541020+NatesanG@users.noreply.github.com>
2024-09-04 18:05:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,20 +25,7 @@ window.addEventListener('scroll', function () {
     }
 });
 
-function loadMoreProjects() {
-    const projectContainer = document.getElementById('projects');
 
-
-    const newProject = document.createElement('div');
-    newProject.className = 'p-1';
-    newProject.innerHTML = `
-        <video src="your-video-source.mp4" controls></video>
-        <p>Your new project description goes here. This content is dynamically loaded as you scroll.</p>
-        <a href="#">View Project</a>
-    `;
-
-    projectContainer.appendChild(newProject);
-}
 
 
 const skillSections = document.querySelectorAll('.col-1, .col-2, .col-3, .col-4, .col-5, .col-6, .col-7, .col-8');
